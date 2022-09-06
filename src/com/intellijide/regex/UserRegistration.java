8fafb25c300bf445a8fc2ea6920674382e56c5cc
AniketDShelar <theaniketshelar@gmail.com>
2022-09-06 14:38:35 +0530

    valid first name

--- src/com/intellijide/regex/UserRegistration.java
+++ src/com/intellijide/regex/UserRegistration.java
@@ -1,18 +1,25 @@
 package com.intellijide.regex;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    //abc.xyz@bl.co.in
-    // abc, bl and co mandatory
-    // xyz and in are not mandatory
-    static Pattern pattern = Pattern.compile("^(abc)[.]?[a-z]*[@](bl)[.](co)[.]?[a-z]*$");
-    public static void main(String[] args) {
-        Matcher matcher = pattern.matcher("abc.xyz@bl.co.in");
-        if(matcher.matches())
+    static Scanner scan = new Scanner(System.in);
+    public void firstName() {
+        System.out.println("Enter first Name : ");
+        String firstName = scan.next();
+        Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{2,}");
+        Matcher matcher1 = pattern1.matcher(firstName);
+        if (matcher1.matches()){
             System.out.println(true);
-        else
+    }else{
             System.out.println(false);
+            System.out.println("Enter Name in valid format, first letter should be capital case and remaining in small case");
+    }
+}
+    public static void main(String[] args) {
+        UserRegistration user = new UserRegistration();
+        user.firstName();
     }
 }
