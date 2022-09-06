63d944f86b46bd3295fec1fc2cf487cbe23a6d3e
AniketDShelar <theaniketshelar@gmail.com>
2022-09-06 13:25:23 +0530

    Valid first name

---
+++ src/com/intellijide/regex/Regex.java
@@ -0,0 +1,17 @@
+package com.intellijide.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    static Pattern pattern = Pattern.compile("^[A-Z]+[a-z]{3,}");
+
+    public static void main(String[] args) {
+        Matcher matcher = pattern.matcher("Aniket");
+        if(matcher.matches())
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
