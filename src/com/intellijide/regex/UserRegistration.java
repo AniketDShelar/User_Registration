b8909af2cecc214307e4f619549196b5b0439b5c
AniketDShelar <theaniketshelar@gmail.com>
2022-09-06 14:13:34 +0530

    valid email id

--- src/com/intellijide/regex/UserRegistration.java
+++ src/com/intellijide/regex/UserRegistration.java
@@ -7,7 +7,7 @@ public class UserRegistration {
     //abc.xyz@bl.co.in
     // abc, bl and co mandatory
     // xyz and in are not mandatory
-    static Pattern pattern = Pattern.compile("^(abc)[.]*[a-z]*[@](bl)[.](co)[.]*[a-z]*$");
+    static Pattern pattern = Pattern.compile("^(abc)[.]?[a-z]*[@](bl)[.](co)[.]?[a-z]*$");
     public static void main(String[] args) {
         Matcher matcher = pattern.matcher("abc.xyz@bl.co.in");
         if(matcher.matches())
