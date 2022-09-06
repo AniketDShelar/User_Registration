ab93871fda0e3d40aaa95df220f7799ee0befd07
AniketDShelar <theaniketshelar@gmail.com>
2022-09-06 13:29:54 +0530

    Valid first name

--- src/com/intellijide/regex/UserRegistration.java
+++ src/com/intellijide/regex/UserRegistration.java
@@ -3,10 +3,8 @@ package com.intellijide.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Regex {
-
+public class UserRegistration {
     static Pattern pattern = Pattern.compile("^[A-Z]+[a-z]{3,}");
-
     public static void main(String[] args) {
         Matcher matcher = pattern.matcher("Aniket");
         if(matcher.matches())
